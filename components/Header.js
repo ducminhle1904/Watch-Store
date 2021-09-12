5c18597a97eddc2580eba67753f2b3ce6b7c4ede
Minh Đức <ducle7144@gmail.com>
2021-09-12 10:50:17 +0700

    Optimize Speed

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,6 @@ function Header() {
     return () => window.removeEventListener("scroll", transitionNavBar);
   }, []);
 
-  if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   return (
     <div
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+import useInView from "react-cool-inview";
 import Head from "next/head";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -6,15 +8,20 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import FeaturesBar from "../components/FeaturesBar";
 import Collections from "../components/Collections";
-import BestSellers from "../components/BestSellers";
+const BestSellers = dynamic(() => import("../components/BestSellers"));
 import Promo from "../components/Promo";
-import Design from "../components/Design";
-import PrimeCollections from "../components/PrimeCollections";
+const Design = dynamic(() => import("../components/Design"));
+const PrimeCollections = dynamic(() =>
+  import("../components/PrimeCollections")
+);
 import Intro from "../components/Intro";
 import Articles from "../components/Articles";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => unobserve(), // only run once
+  });
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -31,7 +38,7 @@ export default function Home() {
         <Banner />
         <FeaturesBar />
         <Collections />
-        <BestSellers />
+        <div ref={observe}>{inView && <BestSellers />}</div>
         <Promo
           color="white"
           img="https://res.cloudinary.com/dppndtfp8/image/upload/v1631109902/promo-01_r1rqpg.jpg"
@@ -39,8 +46,8 @@ export default function Home() {
           p="This tale begins with our namesake’s profession: the ambassador. Like all envoys, the original ambassador’s job was to be the manifestation of his homeland on foreign soils. But, he was always different..."
           btn="READ OUR STORY"
         />
-        <Design color="black" />
-        <PrimeCollections />
+        <div ref={observe}>{inView && <Design color="black" />}</div>
+        <div ref={observe}>{inView && <PrimeCollections />}</div>
         <Intro
           title="BE AN AMBASSADOR"
           p="It's all about reputation. That's why we've teamed up with some outstanding ambassadors from all around the globe to aid in our quest for excellence."
